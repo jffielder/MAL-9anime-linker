6c09c395e6411f017253f56005ac60b2a601f9fa
jffielder <jfielder0@gmail.com>
2023-04-09 02:53:37 -0400

    Init commit

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    CLIENT_ID: "0dec2952591b04cf0947d879a5f1cb71",
+    CLIENT_SECRET: "6363bdde98bb333a71f2a7f0268f218b242b300907976db3191f714c39061fb4",
+  }
+  
+  module.exports = config;

e8d9ed36674db652d38c2a732c63556c0727e53f
jffielder <jfielder0@gmail.com>
2023-04-16 16:43:05 -0400

    moved functions into class

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 // MAL to 9anime Linker Extension
-// Copyright (c) 2023 James Fielder
+// Copyright (c) 2023 jff-i
 // Licensed under the MIT License
 
 // This extension adds a link to the corresponding MyAnimeList (MAL) record on a 9anime anime information page. 
@@ -19,7 +19,6 @@ Some things that don't work well and improvements to searching and/or matching f
 - Unreleased ()
 
 */
-MAL9LinkerSearchAndLinkMAL()
 /**
  * Searches for a matching anime title on the 9anime website and adds a link to the MyAnimeList page for that anime.
  *
@@ -28,143 +27,154 @@ MAL9LinkerSearchAndLinkMAL()
  * @example
  * searchAndLinkMAL();
  */
-async function MAL9LinkerSearchAndLinkMAL() {
-  if (window.location.href.includes('9anime') && window.location.href.includes('/watch')) {
+class MAL9Linker {
 
-    var anime_titles_element = document.querySelector('div.names.font-italic.mb-2');
-    var anime_titles_array = anime_titles_element.textContent.split('; ');
-    var matched = false
+  async searchAndLinkMAL() {
+    if (window.location.href.includes('9anime') && window.location.href.includes('/watch')) {
 
-    MAL9LinkerSearchAnime(MAL9RemoveSpecialChars(anime_titles_array[0]))
-      .then(results => {
-        console.log(results)
+      var anime_titles_element = document.querySelector('div.names.font-italic.mb-2');
+      var anime_titles_array = anime_titles_element.textContent.split('; ');
+      var matched = false
 
-        if (Array.isArray(results) && results.length >= 1) {
-          for (let i = 0; i < results.length && !matched; i++) {
-            for (let j = 0; j < anime_titles_array.length && !matched; j++) {
+      this.searchAnime(this.removeSpecialChars(anime_titles_array[0]))
+        .then(results => {
+          console.log(results)
 
-              // compare titles
-              console.log(`Comparing ${results[i].title} and ${anime_titles_array[j]}`)
-              if (compareTitles(results[i], anime_titles_array[j])) {
-                testAnimeURL(results[i].mal_id)
-                  .then(isValidURL => {
-                    if (isValidURL) {
-                      matched = true;
-                      console.log(results[i].url)
-                      addLinkToTitle(results[i].url, anime_titles_element)
+          if (Array.isArray(results) && results.length >= 1) {
+            for (let i = 0; i < results.length && !matched; i++) {
+              for (let j = 0; j < anime_titles_array.length && !matched; j++) {
+
+                // compare titles
+                console.log(`Comparing ${results[i].title} and ${anime_titles_array[j]}`)
+                if (this.compareTitles(results[i], anime_titles_array[j])) {
+                  
+                  // validate link
+                  this.testAnimeURL(results[i].mal_id)
+                    .then(isValidURL => {
+                      if (isValidURL) {
+                        matched = true;
+                        console.log(results[i].url)
+
+                        // link is inserted to title
+                        this.addLinkToTitle(results[i].url, anime_titles_element)
+                      }
                     }
-                  }
-                  )
-                  .catch(error => console.log(error));
+                    )
+                    .catch(error => console.log(error));
+                }
               }
             }
+          } else {
+            console.log("Lookup yielded no results")
           }
-        } else {
-          console.log("Lookup yielded no results")
         }
-      }
-      )
-      .catch(error => console.error(error));
+        )
+        .catch(error => console.error(error));
+    }
+  }
+  /**
+   * Searches for an anime on MyAnimeList using the jikan API based on the given title string.
+   *
+   * @param {string} title_str - The title string to search for on MyAnimeList.
+   * @returns {Promise<Array>} Returns a promise that resolves to an array of anime objects that match the search query.
+   *
+   * @example
+   * searchAnime('Cowboy Bebop').then(anime_list => {
+   *   console.log(`Found ${anime_list.length} anime that match the search query.`);
+   *   console.log(anime_list);
+   * });
+   */
+  async searchAnime(title_str) {
+    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${encodeURIComponent(title_str)}`);
+    const data = await response.json();
+    return data.data;
+  }
+
+
+  /**
+   * Tests the validity of a MyAnimeList URL for a given anime ID.
+   * TODO: If link fails, use this function for defaulting the link to a MAL page, preferably the search results page. 
+   *
+   * @param {number} id - The ID of the anime on MyAnimeList.
+   * @returns {Promise<boolean>} Returns a promise that resolves to true if the anime exists on MyAnimeList, otherwise resolves to false.
+   *
+   * @example
+   * testAnimeURL(1).then(result => {
+   *   if (result) {
+   *     console.log('Anime exists on MyAnimeList');
+   *   } else {
+   *     console.log('Anime does not exist on MyAnimeList');
+   *   }
+   * });
+   */
+  async testAnimeURL(id) {
+    const response = await fetch(`https://api.jikan.moe/v4/anime/${id}`);
+    const data = await response.json();
+    console.log(`testing id: ${id}`)
+    console.log(data)
+    if (data.status === 404) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  /**
+   * Compares the title or title_english property of the result_title object with the page_title string.
+   *
+   * @param {Object} result_title - An object containing the title and title_english properties.
+   * @param {string} page_title - The title string to compare with the result_title.
+   * @returns {boolean} Returns true if the title or title_english property of the result_title object matches with the page_title string, otherwise returns false.
+   *
+   * @example
+   * compareTitles({title: 'The Lion King', title_english: 'The Lion King'}, 'The Lion King');
+   * // Returns true
+   */
+  compareTitles(result_title, page_title) {
+    if (result_title.title === page_title || result_title.title_english === page_title) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Adds a link to the given MAL URL to the anime_title_html_element.
+   *
+   * @param {string} mal_url - The URL of the anime on MyAnimeList.
+   * @param {HTMLElement} anime_title_html_element - The HTML element to which the link will be added.
+   *
+   * @example
+   * var mal_url = 'https://myanimelist.net/anime/1/Cowboy_Bebop';
+   * var anime_title_html_element = document.getElementById('anime-title');
+   * addLinkToTitle(mal_url, anime_title_html_element);
+   */
+  addLinkToTitle(mal_url, anime_title_html_element) {
+    // link creation
+    var link = document.createElement('a');
+    link.href = mal_url;
+    link.textContent = anime_title_html_element.textContent;
+    link.target = "_blank"; // open a new tab
+
+    // transplant
+    anime_title_html_element.textContent = '';
+    anime_title_html_element.appendChild(link);
+  }
+
+  /**
+   * Removes special characters from a string, replacing them with spaces or removing them altogether.
+   * There seems to have been issues searching certain titles when these characters are present.
+   *
+   * @param {string} str - The string to remove special characters from.
+   * @returns {string} Returns the input string with special characters removed and replaced with spaces, or removed altogether.
+   */
+  removeSpecialChars(str) {
+    return str.replace(/[^\w\s]|_/g, " ")
+              .replace(/\s+/g, " ")
+              .replace(/[^\w\s]/g, "");
   }
 }
-/**
- * Searches for an anime on MyAnimeList using the jikan API based on the given title string.
- *
- * @param {string} title_str - The title string to search for on MyAnimeList.
- * @returns {Promise<Array>} Returns a promise that resolves to an array of anime objects that match the search query.
- *
- * @example
- * MAL9LinkerSearchAnime('Cowboy Bebop').then(anime_list => {
- *   console.log(`Found ${anime_list.length} anime that match the search query.`);
- *   console.log(anime_list);
- * });
- */
-async function MAL9LinkerMAL9LinkerSearchAnime(title_str) {
-  const response = await fetch(`https://api.jikan.moe/v4/anime?q=${encodeURIComponent(title_str)}`);
-  const data = await response.json();
-  return data.data;
-}
 
 
-/**
- * Tests the validity of a MyAnimeList URL for a given anime ID.
- * TODO: Use this function to link to MAL, preferably the search results page. 
- *
- * @param {number} id - The ID of the anime on MyAnimeList.
- * @returns {Promise<boolean>} Returns a promise that resolves to true if the anime exists on MyAnimeList, otherwise resolves to false.
- *
- * @example
- * testAnimeURL(1).then(result => {
- *   if (result) {
- *     console.log('Anime exists on MyAnimeList');
- *   } else {
- *     console.log('Anime does not exist on MyAnimeList');
- *   }
- * });
- */
-async function MAL9LinkerTestAnimeURL(id) {
-  const response = await fetch(`https://api.jikan.moe/v4/anime/${id}`);
-  const data = await response.json();
-  console.log(`testing id: ${id}`)
-  console.log(data)
-  if (data.status === 404) {
-    return false
-  } else {
-    return true
-  }
-}
-
-/**
- * Compares the title or title_english property of the result_title object with the page_title string.
- *
- * @param {Object} result_title - An object containing the title and title_english properties.
- * @param {string} page_title - The title string to compare with the result_title.
- * @returns {boolean} Returns true if the title or title_english property of the result_title object matches with the page_title string, otherwise returns false.
- *
- * @example
- * compareTitles({title: 'The Lion King', title_english: 'The Lion King'}, 'The Lion King');
- * // Returns true
- */
-function MAL9LinkerCompareTitles(result_title, page_title) {
-  if (result_title.title === page_title || result_title.title_english === page_title) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-/**
- * Adds a link to the given MAL URL to the anime_title_html_element.
- *
- * @param {string} mal_url - The URL of the anime on MyAnimeList.
- * @param {HTMLElement} anime_title_html_element - The HTML element to which the link will be added.
- *
- * @example
- * var mal_url = 'https://myanimelist.net/anime/1/Cowboy_Bebop';
- * var anime_title_html_element = document.getElementById('anime-title');
- * addLinkToTitle(mal_url, anime_title_html_element);
- */
-function MAL9LinkerAddLinkToTitle(mal_url, anime_title_html_element) {
-  // link creation
-  var link = document.createElement('a');
-  link.href = mal_url;
-  link.textContent = anime_title_html_element.textContent;
-  link.target = "_blank"; // open a new tab
-
-  // transplant
-  anime_title_html_element.textContent = '';
-  anime_title_html_element.appendChild(link);
-}
-
-/**
- * Removes special characters from a string, replacing them with spaces or removing them altogether.
- * There seems to have been issues searching certain titles when these characters are present.
- *
- * @param {string} str - The string to remove special characters from.
- * @returns {string} Returns the input string with special characters removed and replaced with spaces, or removed altogether.
- */
-function MAL9LinkerMAL9RemoveSpecialChars(str) {
-  return str.replace(/[^\w\s]|_/g, " ")
-            .replace(/\s+/g, " ")
-            .replace(/[^\w\s]/g, "");
-}
+const m9 = new MAL9Linker();
+m9.searchAndLinkMAL();
